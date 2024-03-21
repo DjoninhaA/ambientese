923c619c904bcbe7a3dcf63f34463fa142493561
DjoninhaA <djonathan8016@gmail.com>
2024-03-20 21:14:14 -0300

    Arrumado JPA

---
+++ ambientese/src/main/java/com/Integrador/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.Integrador.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Integrador.ambientese.model.Login;
+
+public interface LoginRepository extends JpaRepository<Login, Long> {
+    
+} 
