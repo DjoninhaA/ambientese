9289ceca7f920a8b37c5fdfe6de0f452b559037a
DjoninhaA <djonathan8016@gmail.com>
2024-04-25 00:07:43 -0300

    arrumando model e controler

--- ambientese/src/main/java/com/Integrador/ambientese/interfac/EmpresaRepository.java
+++ ambientese/src/main/java/com/Integrador/ambientese/interfac/EmpresaRepository.java
@@ -1,9 +1,7 @@
 package com.Integrador.ambientese.interfac;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.Integrador.ambientese.model.Empresa;
 
-public interface EmpresaRepository extends JpaRepository<EmpresaRepository, Long> {
-    Empresa findAll(String userName);
-    
-}
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {}
