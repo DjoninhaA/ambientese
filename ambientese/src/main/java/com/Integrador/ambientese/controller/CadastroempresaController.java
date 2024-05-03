953ceb68bc8eb3664efa51b584a84d3c2eedd50c
DjoninhaA <djonathan8016@gmail.com>
2024-05-02 22:23:21 -0300

    atualizando cadastroEmpresaController

--- ambientese/src/main/java/com/Integrador/ambientese/controller/CadastroempresaController.java
+++ ambientese/src/main/java/com/Integrador/ambientese/controller/CadastroempresaController.java
@@ -1,19 +1,18 @@
 package com.Integrador.ambientese.controller;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.Integrador.ambientese.interfac.EmpresaRepository;
 import com.Integrador.ambientese.model.Empresa;
 
-@Controller
+@RestController
 @RequestMapping("/empresa")
 public class CadastroempresaController {
     @Autowired
@@ -23,25 +22,27 @@ public class CadastroempresaController {
     public String home() {
         return "cadastroEmpresa"; // Retorna o nome do arquivo HTML sem a extensão
     }
-      @PostMapping("/")
-    public ResponseEntity<Empresa> PostUser(@RequestBody Empresa empresa ){
-        empresaRepository.save(empresa);
-        return ResponseEntity.ok(empresa);
-    }
 
 
 
-    @GetMapping("/buscar-empresa")
-    public ResponseEntity<List<Empresa>>GetAll(){
-        List<Empresa>allEmpresas = empresaRepository.findAll();
-        return ResponseEntity.ok(allEmpresas);
-    }
+    // @GetMapping("/buscaEmpresa")
+    // public ResponseEntity<List<Empresa>>GetAll(){
+    //     List<Empresa>allEmpresas = empresaRepository.findAll();
+    //     return ResponseEntity.ok(allEmpresas);
+    // }
 
-    @GetMapping("/")
+    @GetMapping("/buscaEmpresa")
     public ResponseEntity<Empresa> GetByName(@PathVariable String nome_fantasia){
         Empresa empresa = empresaRepository.findByNomeFantasia(nome_fantasia);
         return ResponseEntity.ok(empresa);
     }
+
+    @PostMapping("/")
+    public ResponseEntity<Empresa> PostEmpresa(@RequestBody Empresa empresa){
+        empresaRepository.save(empresa);
+        return ResponseEntity.ok(empresa);
+    }
+
 
 }
 
