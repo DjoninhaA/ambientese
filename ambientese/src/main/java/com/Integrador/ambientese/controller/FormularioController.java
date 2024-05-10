78ccaa5b6327162694195a79f47449390eb9e2a0
@ctrlVi <otvri13@gmail.com>
2024-05-09 21:05:55 -0300

    alterando tela de formulario

--- ambientese/src/main/java/com/Integrador/ambientese/controller/FormularioController.java
+++ ambientese/src/main/java/com/Integrador/ambientese/controller/FormularioController.java
@@ -2,12 +2,21 @@ package com.Integrador.ambientese.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class FormularioController {
 
     @GetMapping("/formulario")
-    public String home() {
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("formData", new FormData());
         return "formularioEmpresa"; // Retorna o nome do arquivo HTML sem a extensão
+    }
+
+    @PostMapping("/formulario")
+    public String processarFormulario(@ModelAttribute("formData") FormData formData) {
+        return "resultadoFormulario"; //criar tela de resultado (ex:formulario enviado com sucesso!)
     }
 }
